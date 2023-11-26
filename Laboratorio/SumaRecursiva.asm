e1fe84ac5b88ba66d669734d913cabe4edb05c70
WillGonz91 <139039816+WillGonz91@users.noreply.github.com>
2023-11-26 18:19:27 -0400

    Add files via upload

---
+++ Laboratorio/SumaRecursiva.asm
@@ -0,0 +1,39 @@
+.data
+	message: .asciiz "Ingrese un numero: "
+	resultado: .asciiz "El resultado es: "
+.text
+.globl main
+	main:
+		li $v0, 4
+		la $a0, message
+		syscall
+		
+		li $v0, 5
+		syscall
+		
+		move $a0, $v0
+		
+		jal sum
+		
+		li $v0, 4
+		la $a0, resultado
+		syscall
+		
+		li $v0, 1
+		move $a0, $t0
+		syscall
+		
+	exit:
+		li $v0, 10
+		syscall
+		
+	sum:
+		slti $t2, $a0, 1
+		beq $a0, $zero, sum_exit
+		add $a1, $a1, $a0
+		addi $a0, $a0, -1
+		j sum
+		
+		sum_exit:
+			add $t0, $a1, $zero
+			jr $ra		
